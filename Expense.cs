0117edca7f5f1d2c0c492361d0f3d6fdec7cb1e3
nitzan933 <nitzan963@gmail.com>
2023-10-15 15:13:44 +0300

    continual and payments implement

---
+++ Expense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.src
+{
+    public class Expense
+    {
+        public int id {  get; set; }
+        public bool income { get; set; } = false;
+        public string description { get; set; } = "";
+        public string category { get; set; } = "";
+        public double amount { get; set; } = 0;
+        public string date {  get; set; } = DateTime.Now.ToString("dd/MM/yyyy");
+        public int[] payments { get; set; } = {1,1};
+        public bool continual { get; set; } = false;
+        public string comments { get; set; } = "";
+    }
+}
--- src/MontlyExpenses.cs
+++ src/MontlyExpenses.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using ExpenseManager.src;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using ComboBox = System.Windows.Forms.ComboBox;
 
@@ -18,8 +19,11 @@ namespace ExpenseManager
 
     public partial class MontlyExpenses : Form
     {
-        public Home homeForm;
-        public string folderPath { get; set; }
+        private Home homeForm;
+        private string folderPath { get; set; }
+        private Boolean tablesave_flag = true;
+        private Dictionary<String, List<Expense>> lmonths = new Dictionary<String, List<Expense>>();
+        private BindingList<String> BLmonths = new BindingList<String>();
         public MontlyExpenses(Home homeForm)
         {
             InitializeComponent();
@@ -29,16 +33,80 @@ namespace ExpenseManager
 
         private void MontlyExpenses_FormClosing(object sender, FormClosingEventArgs e)
         {
-            Application.Exit();
+            if (tablesave_flag == false)
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to save your changes?", "ALERT", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+            Environment.Exit(1);
         }
-
-        private Boolean tablesave_flag = true;
 
         private void home_Click(object sender, EventArgs e)
         {
             homeForm.formState.monthlyExpenseForm = this;
             this.Hide();
             homeForm.Show();
+        }
+
+        private void MontlyExpenses_Load(object sender, EventArgs e)
+        {
+            //loading months to combobox
+            DirectoryInfo d = new DirectoryInfo(folderPath);
+            FileInfo[] Files = d.GetFiles("*.csv"); //Getting csv files
+            foreach (FileInfo file in Files)
+            {
+                String month = file.Name.Split('.')[0];
+                lmonths.Add(month, new List<Expense>());
+                BLmonths.Insert(0, month);
+                String fileName = folderPath + "\\" + month + ".csv";
+                String data;
+                try
+                {
+                    data = File.ReadAllText(fileName);
+                }
+                catch
+                {
+                    return;
+                }
+                String[] rows = data.Split('\n');
+
+                for (int i = 0; i < rows.Length - 1; i++)
+                {
+                    String row = rows[i];
+                    String[] cells = row.Split(',');
+                    Expense expense = new Expense();
+                    expense.income = Convert.ToBoolean(cells[0]);
+                    expense.description = Convert.ToString(cells[1]);
+                    expense.category = Convert.ToString(cells[2]);
+                    expense.amount = Convert.ToDouble(cells[3]);
+                    expense.date = Convert.ToString(cells[4]);
+                    expense.payments = Array.ConvertAll(cells[5].Split(' '), s => Convert.ToInt32(s));
+                    expense.continual = Convert.ToBoolean(cells[6]);
+                    expense.comments = Convert.ToString(cells[7]);
+                    lmonths[month].Add(expense);
+                }
+
+            }
+
+            this.months.DataSource = BLmonths;
+            if (this.months.Items.Count > 0)
+            {
+                this.months.SelectedIndex = 0;
+
+            }
+
+            //loading the categoris
+            DataGridViewComboBoxColumn category = this.category;
+            String[] categories = { "food", "clothes", "bills", "leasure" };
+            foreach (String val in categories)
+            {
+                category.Items.Add(val);
+            }
+
         }
 
         private void ExpensesByMonth_DefaultValuesNeeded(object sender, System.Windows.Forms.DataGridViewRowEventArgs e)
@@ -48,45 +116,42 @@ namespace ExpenseManager
             e.Row.Cells["category"].Value = "";
             e.Row.Cells["amount"].Value = 0;
             e.Row.Cells["date"].Value = DateTime.Now.ToString("dd/MM/yyyy");
-            e.Row.Cells["payments"].Value = 1;
+            e.Row.Cells["payments"].Value = "1 1";
             e.Row.Cells["continual"].Value = false;
             e.Row.Cells["comments"].Value = "";
         }
 
-        private void save_Click(object sender, EventArgs e)
-        {
-            String month = this.months.Text;
-            String fileName = folderPath + "\\" + month + ".csv";
-            DataGridView table = this.ExpensesByMonth;
-            string[] outputCsv = new string[table.Rows.Count-1];
-            try
-            {
-                for (int i = 0; i < table.Rows.Count-1; i++)
-                {
-                    for (int j = 0; j < table.Columns.Count; j++)
-                    {
-                        var val = table.Rows[i].Cells[j].Value;
-                        outputCsv[i] += val.ToString() + ",";
-
-                    }
-                }
-                File.WriteAllLines(fileName, outputCsv, Encoding.UTF8);
-                System.Windows.Forms.MessageBox.Show("Your data is saved.");
-                tablesave_flag = true;
-            }
-            catch
-            {
-                System.Windows.Forms.MessageBox.Show("There was an error while saving your data. Please try again.");
-            }
-        }
+        
 
         private void months_SelectionChangeCommitted(object sender, EventArgs e)
         {
             if(this.months.Text.Equals("") == false && this.months.Items.Contains(this.months.Text) == false)
             {
-                lmonths.Insert(0, this.months.Text.ToString());
+                try
+                {
+                    lmonths.Add(this.months.Text.ToString(), new List<Expense>());
+                    BLmonths.Insert(0, this.months.Text.ToString());
+                } catch { return; }
                 this.months.SelectedIndex = 0;
                 this.ExpensesByMonth.Rows.Clear();
+
+                //add continual expenses from prev month
+                if (BLmonths.Count > 1) {
+                    string month = BLmonths[1];
+                    for (int i = 0; i < lmonths[month].Count; i++)
+                    {
+                        Expense expense = lmonths[month][i];
+                        if (expense.continual)
+                        {
+                            this.ExpensesByMonth.Rows.Add(expense.income, expense.description, expense.category, expense.amount, expense.date, expense.payments[0] + " " + expense.payments[1], true, expense.comments);
+                        }
+                        if (expense.payments[1] < expense.payments[0])
+                        {
+                            string payments = expense.payments[0] + " " + (expense.payments[1]+1);
+                            this.ExpensesByMonth.Rows.Add(expense.income, expense.description, expense.category, expense.amount, expense.date, payments, false, expense.comments);
+                        }
+                    }
+                }
             }
         }
 
@@ -94,14 +159,14 @@ namespace ExpenseManager
         {
             if (tablesave_flag == false)
             {
-                DialogResult dialogResult = MessageBox.Show("Are you sure you don't want to save your changes?", "ALERT", MessageBoxButtons.YesNo);
-                if (dialogResult == DialogResult.No)
+                DialogResult dialogResult = MessageBox.Show("Do you want to save your changes?", "ALERT", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
                 {
                     return;
                 }
             }
             
-            String month = this.months.SelectedItem.ToString();
+            String month = this.months.Text;
             String fileName = folderPath + "\\" + month + ".csv";
             String data;
             try
@@ -137,36 +202,7 @@ namespace ExpenseManager
 
         }
 
-        private BindingList<String> lmonths = new BindingList<String>();
-
-        private void MontlyExpenses_Load(object sender, EventArgs e)
-        {
-            //loading months to combobox
-            ComboBox months = this.months;
-            DirectoryInfo d = new DirectoryInfo(folderPath); 
-            FileInfo[] Files = d.GetFiles("*.csv"); //Getting csv files
-
-            foreach (FileInfo file in Files)
-            {
-                lmonths.Add(file.Name.Split('.')[0]);
-            }
-            months.DataSource = lmonths;
-            if(months.Items.Count > 0)
-            {
-                months.SelectedIndex = 0;
-
-            }
-
-
-            //loading the categoris
-            DataGridViewComboBoxColumn category = this.category;
-            String[] categories = { "food", "clothes", "bills" , "leasure"};
-            foreach (String val in categories)
-            {
-                category.Items.Add(val);
-            }
-
-        }
+        
 
         private void ExpensesByMonth_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
@@ -175,23 +211,28 @@ namespace ExpenseManager
             {
                 sumAmount();
             }
+            if (this.ExpensesByMonth.Columns[e.ColumnIndex].Name == "payments" )
+            {
+                this.ExpensesByMonth.Rows[e.RowIndex].Cells[e.ColumnIndex].Value += " 1";
+            }
         }
 
         private void sumAmount()
-        {
-            DataGridView table = this.ExpensesByMonth;
-            
+        {            
             double total = 0;
-            foreach (DataGridViewRow row in table.Rows)
+            foreach (DataGridViewRow row in this.ExpensesByMonth.Rows)
                 try
                 {
+                    int payments = 1;
+                    if (row.Cells["payments"].Value != null)
+                        payments = Convert.ToInt32(row.Cells["payments"].Value.ToString().Split(' ')[0]);
                     if (Convert.ToBoolean(row.Cells["income"].Value))
                     {
-                        total += Convert.ToDouble(row.Cells["amount"].Value);
+                        total += Convert.ToDouble(row.Cells["amount"].Value)/payments;
                     }
                     else
                     {
-                        total -= Convert.ToDouble(row.Cells["amount"].Value);
+                        total -= Convert.ToDouble(row.Cells["amount"].Value)/payments;
                     }
                 } catch { }
                 
@@ -205,6 +246,47 @@ namespace ExpenseManager
             }
             this.total.Text = total.ToString();
         }
+        private void save_Click(object sender, EventArgs e)
+        {
+            String month = this.months.Text;
+            String fileName = folderPath + "\\" + month + ".csv";
+            DataGridView table = this.ExpensesByMonth;
+            string[] outputCsv = new string[table.Rows.Count - 1];
+            try
+            {
+                for (int i = 0; i < table.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < table.Columns.Count; j++)
+                    {
+                        var val = table.Rows[i].Cells[j].Value;
+                        outputCsv[i] += val.ToString() + ",";
+                    }
+                }
+                File.WriteAllLines(fileName, outputCsv, Encoding.UTF8);
+                System.Windows.Forms.MessageBox.Show("Your data is saved.");
+                tablesave_flag = true;
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("There was an error while saving your data. Please try again.");
+                return;
+            }
+            for (int i = 0; i < table.Rows.Count - 1; i++)
+            {
+                Expense expense = new Expense();
+                expense.income = Convert.ToBoolean(table.Rows[i].Cells["income"].Value);
+                expense.description = table.Rows[i].Cells["description"].Value.ToString();
+                expense.category = table.Rows[i].Cells["category"].Value.ToString();
+                expense.amount = Convert.ToDouble(table.Rows[i].Cells["amount"].Value);
+                expense.date = table.Rows[i].Cells["date"].Value.ToString();
+                expense.payments = Array.ConvertAll(table.Rows[i].Cells["payments"].Value.ToString().Split(' '), s => Convert.ToInt32(s));
+                expense.continual = Convert.ToBoolean(table.Rows[i].Cells["continual"].Value);
+                expense.comments = table.Rows[i].Cells["comments"].Value.ToString();
+                lmonths[month].Add(expense);
+            }
+
+        }
+
 
     }
 }
